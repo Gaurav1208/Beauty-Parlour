fa92feb3af69ebaa6a2d351a6c4c58641ebd170e
Naseem Ali <naseem.ali.925@gmail.com>
2019-09-10 22:36:40 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/beautyparlour/activities/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.beautyparlour.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.beautyparlour.R;
+import com.example.beautyparlour.welcome;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), welcome.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
